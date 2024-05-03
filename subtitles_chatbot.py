0266301c5491d8a53b8c459e46b3bc175bed2ea3
Elios1597 <100914926+Elios1597@users.noreply.github.com>
2024-05-03 23:02:41 +0200

    Update subtitles_chatbot.py

--- subtitles_chatbot.py
+++ subtitles_chatbot.py
@@ -1,76 +1,50 @@
-import os
-import pysrt
-from dotenv import load_dotenv
-import re
-from concurrent.futures import ThreadPoolExecutor
-from langchain_chroma import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+<!DOCTYPE html>
 
-# Document class to handle text documents and optional metadata.
-class Document:
-    """ Represents a document with content and optional metadata. """
-    def __init__(self, page_content, metadata=None):
-        self.page_content = page_content
-        self.metadata = metadata if metadata is not None else {}
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subtitles Chatbot</title>
+    <link rel="stylesheet" href="../../static/style-chatbot.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google Fonts Link For Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <script src="../../static/script-subtitles.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="{{url_for('static', filename='search.js')}}"></script>
 
-# Main function to process and embed subtitle files.
-def files_embedding() :
+    <div id="header">
+      <h1><a href="/">Anime Atlas</a></h1>
+      <h2><a href="/recommendations">Recommend me</a></h2>
+      <h2><a href="/home-chatbot">Chat</a></h2>
+      <form action="/description" method="post">
+          <input type="text" name="Mod_name" placeholder="Enter anime name" list="anime-suggestions">
+          <datalist id="anime-suggestions">
+              <!-- Suggestions will be dynamically filled here -->
+          </datalist>
+          <button type="submit" class="search-button">Search</button>
+      </form>
+    </div>
 
-    load_dotenv()
-
-    # Function to load and clean subtitle text from .srt files.
-    def load_and_process_subtitle(file_path):
-        """ Load subtitle files, clean their text, and return cleaned text. """
-        try:
-            subs = pysrt.open(file_path, encoding='utf-8')
-            cleaned_text = "\n".join(re.sub('<.*?>', '', sub.text).strip() for sub in subs)
-            return re.sub(r'\n+', '\n', cleaned_text).strip()
-        except Exception as e:
-            print(f"Failed to process {file_path}: {e}")
-            return None
-
-    # Function to load texts from all .srt files in a directory.
-    def load_texts_from_directory(directory):
-        """ Walk through a directory and process all .srt files. """
-        texts = []
-        for root, dirs, files in os.walk(directory):
-            for filename in files:
-                if filename.endswith(".srt"):
-                    file_path = os.path.join(root, filename)
-                    text = load_and_process_subtitle(file_path)
-                    if text:
-                        texts.append(text)
-        return texts
-
-    # Function to process texts in parallel, splitting them into smaller chunks.
-    def parallel_process_texts(texts):
-        """ Use parallel processing to split texts into smaller chunks for better handling. """
-        splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=300)
-        with ThreadPoolExecutor() as executor:
-            chunks = list(executor.map(splitter.split_text, texts))
-        return [item for sublist in chunks for item in sublist]
-
-    # Prompt template for contextualizing questions based on chat history.
-    all_texts = load_texts_from_directory('static/subtitles')
-    documents = [Document(content) for content in parallel_process_texts(all_texts)]
-    vectorstore = Chroma.from_documents(documents=documents, embedding=OpenAIEmbeddings())
+  </head>
+  <body>
+    <!-- Chats container -->
+    <div class="chat-container"></div>
     
-    return vectorstore
+    <!-- Typing container -->
+    <div class="typing-container">
+      <div class="typing-content">
+        <div class="typing-textarea">
+          <textarea id="chat-input" spellcheck="false" placeholder="Enter your question here" required></textarea>
+          <span id="send-btn" class="material-symbols-rounded">send</span>
+        </div>
+        <div class="typing-controls">
+          <span id="theme-btn" class="material-symbols-rounded">light_mode</span>
+          <span id="delete-btn" class="material-symbols-rounded">delete</span>
+      </div>
+    
+    </div>
 
-# Prompt template for answering questions using retrieved context.
-contextualize_q_system_prompt = """Given a chat history and the latest user question \
-which might reference context in the chat history, formulate a standalone question \
-which can be understood without the chat history. Do NOT answer the question, \
-just reformulate it if needed and otherwise return it as is."""
-
-# Function to retrieve or create a chat message history for a session.
-qa_system_prompt = """"You are an assistant specialized in anime and manga. \
-Review the question:
-- If it mentions specific anime or manga or seeks information about them, use your knowledge and the provided context to give a concise, informative answer.
-- If the question is about identifying an anime or manga based on described characteristics (e.g., genre, plot elements), provide a recommendation based on your knowledge and the provided context.
-- If the question does not relate to anime or manga, simply respond that you do not have the answer.
-
-Limit your response to three sentences and avoid mentioning the context in your answer. Strive for a human-like and engaging response.
-
-{context}"""
+  </body>
+</html>
